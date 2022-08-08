a17ec976aeab1ab085255d2302a6f71e2afc45e3
Jordan Jennings <jordanjennings12@gmail.com>
2022-08-08 17:09:51 -0400

    commit tree f9b5b4d25c8047dd765af9a2ba9c8cf1153dd941

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-Hello
+function receivesAFunction(callback){
+    callback();
+
+}
+
+function returnsANamedFunction(){
+    const sayHello = () => "Hello!";
+
+    return sayHello;
+}
+
+function returnsAnAnonymousFunction(){
+
+    return function () {
+        console.log(5+9);
+    }
+}
